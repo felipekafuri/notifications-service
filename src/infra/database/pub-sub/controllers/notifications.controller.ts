ed4a39c9a4fe1e32efe0b5442fad645ab426deff
Felipe Kafuri <felipe11.rk@gmail.com>
2023-01-13 11:35:52 -0300

    .

--- src/infra/database/pub-sub/controllers/notifications.controller.ts
+++ src/infra/database/pub-sub/controllers/notifications.controller.ts
@@ -14,14 +14,24 @@ export class NotificationsController {
   constructor(private sendNotification: SendNotification) {}
 
   @EventPattern()
-  getNotifications(@Payload() data: number[], @Ctx() context: GCPubSubContext) {
+  async handleSendNotification(
+    @Payload() data: number[],
+    @Ctx() context: GCPubSubContext,
+  ) {
     const originalMsg = context.getMessage();
-    // convert Buffer to json
-    const payload = JSON.parse(
+    const { content, category, recipientId } = JSON.parse(
       originalMsg.data.toString(),
     ) as SendNotificationPayload;
 
-    console.log('payload', payload);
+    const { notification } = await this.sendNotification.execute({
+      content,
+      category,
+      recipientId,
+    });
     originalMsg.ack();
+
+    return {
+      notification,
+    };
   }
 }
